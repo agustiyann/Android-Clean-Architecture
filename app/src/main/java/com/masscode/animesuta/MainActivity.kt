fd441fa3f179b494234b1dacb7902e03d0106a4f
agustiyann <agustiyaninfo@gmail.com>
2020-09-24 21:08:42 +0700

    navigation drawer pass

--- app/src/main/java/com/masscode/animesuta/MainActivity.kt
+++ app/src/main/java/com/masscode/animesuta/MainActivity.kt
@@ -2,10 +2,28 @@ package com.masscode.animesuta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navCtrl: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        navCtrl = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navCtrl, drawer_layout)
+        NavigationUI.setupWithNavController(nav_view, navCtrl)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navCtrl, drawer_layout)
     }
 }
