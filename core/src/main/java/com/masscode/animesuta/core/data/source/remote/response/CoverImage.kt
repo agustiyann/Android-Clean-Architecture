45f5c68f7f9c4018058e0563c61a5a00fcd0647e
agustiyann <agustiyaninfo@gmail.com>
2020-10-01 00:00:24 +0700

    add: obfuscation with ProGuard

--- core/src/main/java/com/masscode/animesuta/core/data/source/remote/response/CoverImage.kt
+++ core/src/main/java/com/masscode/animesuta/core/data/source/remote/response/CoverImage.kt
@@ -1,6 +1,10 @@
 package com.masscode.animesuta.core.data.source.remote.response
 
+import com.google.gson.annotations.SerializedName
+
 data class CoverImage(
+    @SerializedName("small")
     val small : String,
+    @SerializedName("large")
     val large : String
 )
--- core/src/main/java/com/masscode/animesuta/core/data/source/remote/response/PosterImage.kt
+++ core/src/main/java/com/masscode/animesuta/core/data/source/remote/response/PosterImage.kt
@@ -1,6 +1,10 @@
 package com.masscode.animesuta.core.data.source.remote.response
 
+import com.google.gson.annotations.SerializedName
+
 data class PosterImage(
+    @SerializedName("medium")
     val medium : String,
+    @SerializedName("large")
     val large : String
 )
